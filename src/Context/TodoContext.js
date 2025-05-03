159fa0740baf3877a48217b039965e7b92db18a2
Shahid22Gujar <shahidahimadgujar86@gmail.com>
2025-05-03 16:22:43 +0530

    code fixed

--- src/Context/TodoContext.js
+++ src/Context/TodoContext.js
@@ -2,7 +2,7 @@ import { useEffect,createContext,useState } from "react";
 export const TodoContext = createContext();
 export const TodoProvider = ({ children }) => {
   const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos'))?localStorage.getItem('todos'):[]);
-
+  console.log(JSON.parse(localStorage.getItem('todos')),type(JSON.parse(localStorage.getItem('todos'))),'shahid')
   useEffect(() => {
     const stored = localStorage.getItem('todos');
     if (stored) setTodos(JSON.parse(stored));
